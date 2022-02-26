0e838179bfc0b318e1ea87006cbab0669c4551b8
Dominic Burkart <code@dominic.computer>
2022-02-26 11:14:54 +0100

    first parser (yields strings)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate lalrpop_util;
+use lalrpop_util::ParseError;
+extern crate proc_macro;
+
+lalrpop_mod!(#[allow(clippy::all)] pub ast);
+
+pub fn glass_parse(s: &str) -> Result<String, ParseError<usize, ast::Token<'_>, &'static str>> {
+    ast::ExprParser::new().parse(s)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(#[allow(clippy::all)] pub grammar);
+lalrpop_mod!(#[allow(clippy::all)] pub ast);
 
 fn main() {
     println!("hi");
 }
 
-#[test]
-fn calculator1() {
-    assert!(grammar::TermParser::new().parse("22").is_ok());
-    assert!(grammar::TermParser::new().parse("(22)").is_ok());
-    assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
-    assert!(grammar::TermParser::new().parse("((22)").is_err());
-}
+// #[test]
+// fn calculator1() {
+// }
