4df1664637ef00385b2b08dc2a66241a19bf7eb5
Dominic Burkart <code@dominic.computer>
2022-04-09 14:29:29 +0200

    implement permutations trait

---
+++ marigold-impl/src/permutations.rs
@@ -0,0 +1,56 @@
+use async_trait::async_trait;
+use futures::stream::Stream;
+use futures::stream::StreamExt;
+use itertools::Permutations;
+
+#[async_trait]
+pub trait Permutable<T> {
+    async fn permutations(
+        self,
+        k: usize,
+    ) -> futures::stream::Iter<Permutations<std::vec::IntoIter<T>>>;
+}
+
+/// This is a glue trait to allow streams to use Permutable in itertools.
+/// The current implementation eagerly consumes the parent stream.
+#[async_trait]
+impl<T, SInput> Permutable<T> for SInput
+where
+    SInput: Stream<Item = T> + Send,
+    T: Clone + Send,
+{
+    async fn permutations(
+        self,
+        k: usize,
+    ) -> futures::stream::Iter<Permutations<std::vec::IntoIter<T>>> {
+        use itertools::Itertools;
+
+        let permutations_iterable = self.collect::<Vec<_>>().await.into_iter().permutations(k);
+        futures::stream::iter(permutations_iterable)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Permutable;
+    use futures::stream::StreamExt;
+
+    #[tokio::test]
+    async fn permutations() {
+        assert_eq!(
+            futures::stream::iter(vec![1, 2, 3])
+                .permutations(2)
+                .await
+                .collect::<Vec<_>>()
+                .await,
+            vec![
+                vec![1, 2],
+                vec![1, 3],
+                vec![2, 1],
+                vec![2, 3],
+                vec![3, 1],
+                vec![3, 2]
+            ]
+        );
+    }
+}
