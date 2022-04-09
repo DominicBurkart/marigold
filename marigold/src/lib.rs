5c06f31b92166617c0a0d56b52787c653fdb276f
Dominic Burkart <code@dominic.computer>
2022-04-09 14:59:59 +0200

    move macro tests in marigold from main to lib

--- marigold/src/lib.rs
+++ marigold/src/lib.rs
@@ -1,8 +1,32 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use marigold_macros::marigold;
+
+    #[tokio::test]
+    async fn chained_permutations_and_combinations() {
+        let r = marigold!(
+            range(0, 2)
+                .permutations(2)
+                .combinations(2)
+                .return
+        )
+        .await
+        .collect::<Vec<_>>();
+        assert_eq!(r, vec![vec![vec![0, 1], vec![1, 0]]]);
+    }
+
+    #[tokio::test]
+    async fn sort_by_external_function() {
+        let sorter = |a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>| a[0].partial_cmp(&b[0]).unwrap();
+        let r = marigold!(
+            range(0, 5)
+                .permutations(3)
+                .combinations(2)
+                .keep_first_n(1, sorter)
+                .return
+        )
+        .await
+        .collect::<Vec<_>>();
+        assert_eq!(r, vec![vec![vec![0, 1, 2], vec![0, 1, 3]]]);
     }
 }
--- marigold/src/main.rs
+++ marigold/src/main.rs
@@ -3,36 +3,3 @@ use anyhow::Result;
 fn main() -> Result<()> {
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use marigold_macros::marigold;
-
-    #[tokio::test]
-    async fn chained_permutations_and_combinations() {
-        let r = marigold!(
-            range(0, 2)
-                .permutations(2)
-                .combinations(2)
-                .return
-        )
-        .await
-        .collect::<Vec<_>>();
-        assert_eq!(r, vec![vec![vec![0, 1], vec![1, 0]]]);
-    }
-
-    #[tokio::test]
-    async fn sort_by_external_function() {
-        let sorter = |a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>| a[0].partial_cmp(&b[0]).unwrap();
-        let r = marigold!(
-            range(0, 5)
-                .permutations(3)
-                .combinations(2)
-                .keep_first_n(1, sorter)
-                .return
-        )
-        .await
-        .collect::<Vec<_>>();
-        assert_eq!(r, vec![vec![vec![0, 1, 2], vec![0, 1, 3]]]);
-    }
-}
