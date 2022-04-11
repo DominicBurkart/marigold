e730fb593376b89c9dfa1e32ad5a5e99cf81a68d
Dominic Burkart <code@dominic.computer>
2022-04-11 18:55:04 +0200

    bump versions

--- publish.sh
+++ publish.sh
@@ -4,7 +4,9 @@ cd marigold-impl && \
   cargo publish && \
   cd ../marigold-grammar && \
   cargo publish && \
+  sleep 60 && \
   cd ../marigold-macros && \
   cargo publish && \
+  sleep 60 && \
   cd ../marigold &&
   cargo publish
