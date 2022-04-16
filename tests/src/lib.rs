e1976713bbed881e07e9848072340e0c360a0cdc
Dominic Burkart <code@dominic.computer>
2022-04-16 11:20:48 +0200

    restructure examples and projects

---
+++ tests/src/lib.rs
@@ -0,0 +1,71 @@
+#[cfg(test)]
+mod tests {
+    use marigold::m;
+
+    #[tokio::test]
+    async fn chained_permutations_and_combinations() {
+        let r = m!(
+            range(0, 2)
+                .permutations(2)
+                .combinations(2)
+                .to_vec()
+                .return
+        )
+        .await;
+        assert_eq!(r, vec![vec![vec![0, 1], vec![1, 0]]]);
+    }
+
+    #[tokio::test]
+    async fn sort_by_external_function() {
+        let sorter = |a: &Vec<i32>, b: &Vec<i32>| a[0].partial_cmp(&b[0]).unwrap();
+        let r = m!(
+            range(0, 2)
+                .permutations(2)
+                .keep_first_n(1, sorter)
+                .to_vec()
+                .return
+        )
+        .await;
+        assert_eq!(r, vec![vec![1, 0]]);
+    }
+
+    #[tokio::test]
+    async fn test_filter() {
+        let is_odd_number = |i: &i32| i % 2 == 1;
+
+        assert_eq!(
+            m!(
+                range(0, 10)
+                .filter(is_odd_number)
+                .to_vec()
+                .return
+            )
+            .await,
+            vec![1, 3, 5, 7, 9]
+        );
+    }
+
+    #[tokio::test]
+    async fn test_permutations_with_replacement() {
+        assert_eq!(
+            m!(
+                range(0, 3)
+                    .permutations_with_replacement(2)
+                    .to_vec()
+                    .return
+            )
+            .await,
+            vec![
+                vec![0, 0],
+                vec![0, 1],
+                vec![0, 2],
+                vec![1, 0],
+                vec![1, 1],
+                vec![1, 2],
+                vec![2, 0],
+                vec![2, 1],
+                vec![2, 2]
+            ]
+        );
+    }
+}
