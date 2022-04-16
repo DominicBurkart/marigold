7fcdd6fe2ecf31f0f264c5db44792b9971960402
Dominic Burkart <code@dominic.computer>
2022-04-16 12:17:16 +0200

    add test in color palette picker example

--- examples/color-palette-picker/src/main.rs
+++ examples/color-palette-picker/src/main.rs
@@ -54,3 +54,29 @@ async fn main() {
         .await
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn limited_palette() {
+        let mod_fifty = |i: &u8| i % 50 == 0;
+        assert_eq!(
+            m!(
+                range(0, 255)
+                .filter(mod_fifty)
+                .permutations_with_replacement(3)
+                .combinations(2)
+                .keep_first_n(2, compare_contrast)
+                .to_vec()
+                .return
+            )
+            .await,
+            vec![
+                vec![vec![0, 0, 0], vec![0, 0, 100]],
+                vec![vec![0, 0, 0], vec![0, 0, 50]]
+            ]
+        );
+    }
+}
