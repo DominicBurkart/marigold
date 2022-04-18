065abc27c051988d65d5cf0f5ee067a83f4a0215
Dominic Burkart <code@dominic.computer>
2022-04-18 11:18:33 +0200

    reintroduce the non-parallel solution and use it in wasm

--- marigold-impl/src/keep_first_n.rs
+++ marigold-impl/src/keep_first_n.rs
@@ -21,6 +21,7 @@ where
     ) -> futures::stream::Iter<std::vec::IntoIter<T>>;
 }
 
+#[cfg(feature = "parallel_stream")]
 #[async_trait]
 impl<SInput, T, F> KeepFirstN<T, F> for SInput
 where
@@ -43,6 +44,7 @@ where
 /// type of the binary heap, which includes a lambda for reversing the ordering fromt the passed
 /// sort_by function. By declaring a new function, we can use generics to describe its type, and
 /// then can use that type while unsafely casting pointers.
+#[cfg(feature = "parallel_stream")]
 async fn impl_keep_first_n<SInput, T, F, FReversed>(
     mut sinput: SInput,
     mut first_n: BinaryHeap<T, binary_heap_plus::FnComparator<FReversed>>,
@@ -111,6 +113,63 @@ where
             .into_sorted_vec()
             .into_iter(),
     )
+}
+
+#[async_trait]
+#[cfg(not(feature = "parallel_stream"))]
+impl<SInput, T, F> KeepFirstN<T, F> for SInput
+where
+    SInput: Stream<Item = T> + Send + Unpin,
+    T: Clone + Send + std::marker::Sync,
+    F: Fn(&T, &T) -> Ordering + std::marker::Send + std::marker::Sync + 'static,
+{
+    async fn keep_first_n(
+        mut self,
+        n: usize,
+        sorted_by: F,
+    ) -> futures::stream::Iter<std::vec::IntoIter<T>> {
+        // use the reverse ordering so that the smallest value is always the first to pop.
+        let mut first_n = BinaryHeap::with_capacity_by(n, |a, b| match sorted_by(a, b) {
+            Ordering::Less => Ordering::Greater,
+            Ordering::Equal => Ordering::Equal,
+            Ordering::Greater => Ordering::Less,
+        });
+
+        while first_n.len() < n {
+            if let Some(item) = self.next().await {
+                first_n.push(item);
+            } else {
+                break;
+            }
+        }
+
+        // If we have exhausted the stream before reaching n values, we can exit early.
+        if first_n.len() < n {
+            return futures::stream::iter(first_n.into_sorted_vec().into_iter());
+        }
+
+        // Otherwise, we can check each remaining value in the stream against the smallest
+        // kept value, updating the kept values only when a keepable value is found.
+        let first_n_mutex = parking_lot::Mutex::new(first_n);
+        let smallest_kept =
+            parking_lot::RwLock::new(first_n_mutex.lock().peek().unwrap().to_owned());
+
+        self.for_each_concurrent(
+            /* arbitrarily set concurrency limit */ 256,
+            |item| async {
+                if sorted_by(&*smallest_kept.read(), &item) == Ordering::Less {
+                    let mut first_n_mut = first_n_mutex.lock();
+                    first_n_mut.pop();
+                    first_n_mut.push(item);
+                    let mut update_smallest_kept = smallest_kept.write();
+                    *update_smallest_kept = first_n_mut.peek().unwrap().to_owned();
+                }
+            },
+        )
+        .await;
+
+        futures::stream::iter(first_n_mutex.into_inner().into_sorted_vec().into_iter())
+    }
 }
 
 #[cfg(test)]
