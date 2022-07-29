fe710ca37eb08bfc6334554e8b23f91614fe4b10
Dominic Burkart <code@dominic.computer>
2022-07-30 00:17:26 +0200

    structs pt1

--- marigold-grammar/src/nodes.rs
+++ marigold-grammar/src/nodes.rs
@@ -1,3 +1,5 @@
+use strum_macros::EnumString;
+
 pub struct StreamNode {
     pub inp: InputFunctionNode,
     pub funs: Vec<StreamFunctionNode>,
@@ -48,4 +50,105 @@ pub struct InputFunctionNode {
 pub struct OutputFunctionNode {
     pub stream_prefix: String,
     pub stream_postfix: String,
+}
+
+pub struct StructDeclarationNode {
+    pub name: String,
+    pub fields: Vec<(String, Primitive)>,
+}
+
+impl StructDeclarationNode {
+    pub fn code(&self) -> String {
+        let name = &self.name;
+        let mut struct_rep = format!(
+            "
+        #[derive(Copy, Clone, Debug, PartialEq)]
+        struct {name} {{
+        "
+        ); // todo add serde de/serialize iff io feature included
+        for (field_name, field_type) in &self.fields {
+            struct_rep.push_str(field_name.as_str());
+            struct_rep.push(':');
+            struct_rep.push_str(primitive_to_type_str(*field_type));
+            struct_rep.push_str(",\n");
+        }
+        struct_rep.push('}');
+        struct_rep
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, EnumString)]
+pub enum Primitive {
+    #[strum(serialize = "u8")]
+    U8,
+    #[strum(serialize = "u16")]
+    U16,
+    #[strum(serialize = "u32")]
+    U32,
+    #[strum(serialize = "u64")]
+    U64,
+    #[strum(serialize = "u128")]
+    U128,
+    #[strum(serialize = "usize")]
+    USize,
+    #[strum(serialize = "i8")]
+    I8,
+    #[strum(serialize = "i16")]
+    I16,
+    #[strum(serialize = "i32")]
+    I32,
+    #[strum(serialize = "i64")]
+    I64,
+    #[strum(serialize = "i128")]
+    I128,
+    #[strum(serialize = "isize")]
+    ISize,
+    #[strum(serialize = "f32")]
+    F32,
+    #[strum(serialize = "f64")]
+    F64,
+    #[strum(serialize = "bool")]
+    Bool,
+    #[strum(serialize = "char")]
+    Char,
+}
+
+pub fn primitive_to_type_str(p: Primitive) -> &'static str {
+    match p {
+        Primitive::U8 => "u8",
+        Primitive::U16 => "u16",
+        Primitive::U32 => "u32",
+        Primitive::U64 => "u64",
+        Primitive::U128 => "u128",
+        Primitive::USize => "usize",
+        Primitive::I8 => "i8",
+        Primitive::I16 => "i16",
+        Primitive::I32 => "i32",
+        Primitive::I64 => "i64",
+        Primitive::I128 => "i128",
+        Primitive::ISize => "isize",
+        Primitive::F32 => "f32",
+        Primitive::F64 => "f64",
+        Primitive::Bool => "bool",
+        Primitive::Char => "char",
+    }
+}
+
+pub enum PrimitiveValue {
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    USize(usize),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    ISize(isize),
+    F32(f32),
+    F64(f64),
+    Bool(bool),
+    Char(char),
 }
