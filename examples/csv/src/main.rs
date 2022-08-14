1ec0745ff13232ef671b454e252c0be3891e9866
Dominic Burkart <code@dominic.computer>
2022-08-14 21:59:19 +0200

    run all streams at once pt2
    - Supports running multiple non-returning streams and includes improved logic around writing CSVs.
    - Removes to_vec, which introduces non-streams into the marigold type system.

--- examples/csv/src/main.rs
+++ examples/csv/src/main.rs
@@ -30,53 +30,53 @@ fn is_spherical(ship: &Ship) -> bool {
 async fn main() {
     console_subscriber::init();
 
-    // read records by passing a schema struct
-    let ships = m!(
-        read_file("./data/uncompressed.csv", csv, struct=Ship)
-        .ok()
-        .filter(is_spherical)
-        .return
-    )
-    .await
-    .collect::<Vec<_>>()
-    .await;
-    println!("Best classes (deserialized): {:?}", ships);
-
-    // read records by passing a schema struct
-    let ships = m!(
-        read_file("./data/compressed.csv.gz", csv, struct=Ship)
-            .ok_or_panic()
-            .filter(is_spherical)
-            .return
-    )
-    .await
-    .collect::<Vec<_>>()
-    .await;
-    println!("Best classes (deserialized, from compressed): {:?}", ships);
-
-    // declare the struct inside marigold
-    let ships = m!(
-        enum Hull {
-            Spherical = "spherical",
-            Split = "split",
-        }
-
-        struct Vaisseau {
-            class: string_8,
-            hull: Hull,
-        }
-
-        read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
-            .ok_or_panic()
-            .return
-    )
-    .await
-    .collect::<Vec<_>>()
-    .await;
-    println!(
-        "All classes (deserialized, from compressed, using Marigold struc definition): {:?}",
-        ships
-    );
+    // // read records by passing a schema struct
+    // let ships = m!(
+    //     read_file("./data/uncompressed.csv", csv, struct=Ship)
+    //     .ok()
+    //     .filter(is_spherical)
+    //     .return
+    // )
+    // .await
+    // .collect::<Vec<_>>()
+    // .await;
+    // println!("Best classes (deserialized): {:?}", ships);
+    //
+    // // read records by passing a schema struct
+    // let ships = m!(
+    //     read_file("./data/compressed.csv.gz", csv, struct=Ship)
+    //         .ok_or_panic()
+    //         .filter(is_spherical)
+    //         .return
+    // )
+    // .await
+    // .collect::<Vec<_>>()
+    // .await;
+    // println!("Best classes (deserialized, from compressed): {:?}", ships);
+    //
+    // // declare the struct inside marigold
+    // let ships = m!(
+    //     enum Hull {
+    //         Spherical = "spherical",
+    //         Split = "split",
+    //     }
+    //
+    //     struct Vaisseau {
+    //         class: string_8,
+    //         hull: Hull,
+    //     }
+    //
+    //     read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
+    //         .ok_or_panic()
+    //         .return
+    // )
+    // .await
+    // .collect::<Vec<_>>()
+    // .await;
+    // println!(
+    //     "All classes (deserialized, from compressed, using Marigold struct definition): {:?}",
+    //     ships
+    // );
 
     // write records to csv
     m!(read_file("./data/uncompressed.csv", csv, struct=Ship)
@@ -86,30 +86,30 @@ async fn main() {
     )
     .await;
 
-    let v = m!(
-
-        enum Hull {
-            Spherical = "spherical",
-            Split = "split",
-        }
-
-        struct Vaisseau {
-            class: string_8,
-            hull: Hull,
-        }
-
-        read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
-            .ok_or_panic()
-            .return
-
-        read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
-            .ok_or_panic()
-            .write_file("./output/uncompressed.csv", csv)
-    )
-    .await
-    .collect::<Vec<_>>()
-    .await;
-    assert_eq!(v.len(), 3);
+    // let v = m!(
+    //
+    //     enum Hull {
+    //         Spherical = "spherical",
+    //         Split = "split",
+    //     }
+    //
+    //     struct Vaisseau {
+    //         class: string_8,
+    //         hull: Hull,
+    //     }
+    //
+    //     read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
+    //         .ok_or_panic()
+    //         .return
+    //
+    //     read_file("./data/compressed.csv.gz", csv, struct=Vaisseau)
+    //         .ok_or_panic()
+    //         .write_file("./output/uncompressed.csv", csv)
+    // )
+    // .await
+    // .collect::<Vec<_>>()
+    // .await;
+    // assert_eq!(v.len(), 3);
 }
 
 /// Returns a single future, where all computation occurs in a single thread.
