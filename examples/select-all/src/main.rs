d12265fbc75053d841e704ceaa4921c03d19246c
Dominic Burkart <code@dominic.computer>
2022-11-09 23:15:31 +0100

    add select-all example

---
+++ examples/select-all/src/main.rs
@@ -0,0 +1,61 @@
+use marigold::m;
+use marigold::marigold_impl::StreamExt;
+use std::collections::HashSet;
+
+async fn run() {
+    let ints_with_duplicates = m!(
+        select_all(
+            range(0, 10),
+            range(10, 20),
+            range(20, 30),
+            range(20, 30)
+        ).return
+    )
+    .await
+    .collect::<Vec<i32>>()
+    .await;
+
+    assert_eq!(ints_with_duplicates.len(), 40);
+
+    assert_eq!(
+        ints_with_duplicates.iter().collect::<HashSet<i32>>().await,
+        (0..30).collect::<HashSet<i32>>()
+    );
+
+    println!("result: {:?}", ints_with_duplicates);
+}
+
+#[cfg(feature = "tokio")]
+#[tokio::main]
+async fn main() {
+    run().await
+}
+
+#[cfg(feature = "async-std")]
+#[async_std::main]
+async fn main() {
+    run().await
+}
+
+#[cfg(not(any(feature = "async-std", feature = "tokio")))]
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    run().await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(not(feature = "async-std"))]
+    #[tokio::test]
+    async fn multi_consumer() {
+        run().await;
+    }
+
+    #[cfg(feature = "async-std")]
+    #[async_std::test]
+    async fn multi_consumer() {
+        run().await;
+    }
+}
