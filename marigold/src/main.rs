7bbcc9821e241a9a16d6a897a96170ace7149489
Dominic Burkart <code@dominic.computer>
2022-11-27 00:07:41 +0100

    migrate from cargo-script to cargo-play, as cargo-script has not been updated since 2018

--- marigold/src/main.rs
+++ marigold/src/main.rs
@@ -4,72 +4,99 @@ use anyhow::Result;
 use clap::Parser;
 
 #[cfg(feature = "cli")]
-const MARIGOLD_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-#[cfg(feature = "cli")]
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Path of the marigold file to read
     #[arg(short, long)]
     file: Option<String>,
+
+    /// Weather to enable compiler optimizations
+    #[arg(short, long, default_value_t = false)]
+    release: bool,
 }
 
 #[cfg(not(feature = "cli"))]
 fn main() {
-    eprintln!("marigold needs to be installed with the cli feature (`cargo install --force marigold -F cli`)");
+    eprintln!("Marigold needs to be installed with the cli feature (`cargo install --force marigold -F cli`)");
 }
 
 #[cfg(feature = "cli")]
 fn main() -> Result<()> {
+    use convert_case::{Case, Casing};
     use std::io;
     use std::io::Read;
     use std::io::Write;
-    use std::process::{Command, Stdio};
+    use std::process::Command;
 
     let args = Args::parse();
 
+    let program_name = {
+        let mut file_name = match &args.file {
+            Some(path) => {
+                if path.contains('.') {
+                    let partial = &path[0..path.find('.').unwrap()];
+                    if partial.len() > 1 {
+                        partial.to_string()
+                    } else {
+                        "marigold_program".to_string()
+                    }
+                } else {
+                    path.to_string()
+                }
+            }
+            None => "marigold_program".to_string(),
+        };
+
+        file_name = file_name.to_case(Case::Snake);
+        file_name = file_name
+            .strip_prefix("_")
+            .map(|s| s.to_string())
+            .unwrap_or(file_name);
+        file_name = file_name
+            .strip_suffix("_")
+            .map(|s| s.to_string())
+            .unwrap_or(file_name);
+        format!("{file_name}_")
+    };
+
     let program_contents = match args.file {
-        Some(path) => std::fs::read_to_string(&path)?,
+        Some(path) => std::fs::read_to_string(&path)?.trim().to_string(),
         None => {
             let mut stdin = String::new();
             io::stdin().lock().read_to_string(&mut stdin)?;
-            stdin
+            stdin.trim().to_string()
         }
     };
 
-    let mut file = tempfile::NamedTempFile::new()?;
+    const MARIGOLD_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    let mut file = tempfile::Builder::new()
+        .prefix(&program_name)
+        .suffix(".rs")
+        .tempfile()?;
+
     write!(
         file,
-        r#"#!/usr/bin/env run-cargo-script
-        //! ```cargo
-        //! [package]
-        //! edition = "2021"
-        //!
-        //! [dependencies]
-        //! tokio = {{ version = "1", features = ["full"] }}
-        //! marigold = {{ version="={MARIGOLD_VERSION}", features=["tokio", "io"] }}
-        //! ```
+        r#"//#tokio={{ version="1", features=["full"] }}
+//# marigold={{ version="={MARIGOLD_VERSION}", features=["tokio", "io"] }}
 
-        use marigold::m;
-
-        #[tokio::main]
-        async fn main() {{
-            m!({program_contents}).await;
-        }}
-    "#
+#[tokio::main]
+async fn main() {{ marigold::m!({program_contents}).await }}
+"#
     )?;
 
     let exit_status = Command::new("cargo")
         .args([
-            "script",
+            "play",
+            "--release",
+            "--edition",
+            "2021",
             file.path()
                 .as_os_str()
                 .to_str()
                 .expect("marigold failure: generated non-utf8 file descriptor"),
         ])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
         .spawn()?
         .wait()?;
 
