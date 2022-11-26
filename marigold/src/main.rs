5f4838066dfe45f74d33c4d973fb7a6f2a1772fd
Dominic Burkart <code@dominic.computer>
2022-11-26 16:34:36 +0100

    add v1 for marigold executable

--- marigold/src/main.rs
+++ marigold/src/main.rs
@@ -1,5 +1,67 @@
 use anyhow::Result;
+use clap::Parser;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+const MARIGOLD_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path of the marigold file to read
+    #[arg(short, long)]
+    file: Option<PathBuf>,
+}
 
 fn main() -> Result<()> {
-    Ok(())
+    let args = Args::parse();
+
+    let program_contents = match args.file {
+        Some(path) => std::fs::read_to_string(&path)?,
+        None => {
+            let mut stdin = String::new();
+            io::stdin().lock().read_to_string(&mut stdin)?;
+            stdin
+        }
+    };
+
+    let mut file = tempfile::NamedTempFile::new()?;
+    write!(
+        file,
+        r#"#!/usr/bin/env run-cargo-script
+        //! ```cargo
+        //! [package]
+        //! edition = "2021"
+        //!
+        //! [dependencies]
+        //! tokio = {{ version = "1", features = ["full"] }}
+        //! marigold = {{ version="{MARIGOLD_VERSION}", features=["tokio"] }}
+        //! ```
+
+        use marigold::m;
+
+        #[tokio::main]
+        async fn main() {{
+            m!({program_contents}).await;
+        }}
+    "#
+    )?;
+
+    let exit_status = Command::new("cargo")
+        .args([
+            "script",
+            file.path()
+                .as_os_str()
+                .to_str()
+                .expect("marigold failure: generated non-utf8 file descriptor"),
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?
+        .wait()?;
+
+    std::process::exit(exit_status.code().unwrap_or(0));
 }
