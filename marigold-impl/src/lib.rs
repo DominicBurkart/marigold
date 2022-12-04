f667d6caf8096e5bc316cb0b262a8f7ab9d24197
Dominic Burkart <code@dominic.computer>
2022-12-04 16:41:27 +0100

    partition by hash

--- marigold-impl/src/lib.rs
+++ marigold-impl/src/lib.rs
@@ -7,6 +7,7 @@ pub mod combinations;
 pub mod keep_first_n;
 pub mod marifold;
 pub mod multi_consumer_stream;
+pub mod partition;
 pub mod permutations;
 pub mod run_stream;
 
@@ -14,6 +15,7 @@ pub use collect_and_apply::CollectAndAppliable;
 pub use combinations::Combinable;
 pub use keep_first_n::KeepFirstN;
 pub use marifold::Marifold;
+pub use partition::PartitionByHash;
 pub use permutations::Permutable;
 
 pub use arrayvec;
