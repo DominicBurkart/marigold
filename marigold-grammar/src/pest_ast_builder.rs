141ac264d62391ac45ddb9370e9445ed77db6f3b
dominicburkart <code@dominic.computer>
2026-02-16 21:39:28 +0100

    style(codegen): fix rustfmt line length in filter_map async path

--- marigold-grammar/src/pest_ast_builder.rs
+++ marigold-grammar/src/pest_ast_builder.rs
@@ -898,7 +898,9 @@ impl PestAstBuilder {
         ));
 
         #[cfg(any(feature = "tokio", feature = "async-std"))]
-        return Ok(format!("filter_map(|v| async move {{ {filter_map_fn}(v) }})"));
+        return Ok(format!(
+            "filter_map(|v| async move {{ {filter_map_fn}(v) }})"
+        ));
     }
 
     fn build_permutations_fn(pair: Pair<Rule>) -> Result<String, String> {
