8999142455c63574012ffb73f7b9624196cca9e2
dominicburkart <code@dominic.computer>
2026-06-20 09:44:36 +0200

    fix(style): sort imports in parser_iai.rs for rustfmt

--- marigold-grammar/benches/parser_iai.rs
+++ marigold-grammar/benches/parser_iai.rs
@@ -1,5 +1,5 @@
 use iai_callgrind::{library_benchmark, library_benchmark_group, main};
-use marigold_grammar::parser::{MarigoldParser, MarigoldParseError, PestParser};
+use marigold_grammar::parser::{MarigoldParseError, MarigoldParser, PestParser};
 use std::hint::black_box;
 
 const MULTIPLE_STREAMS: &str = r#"
